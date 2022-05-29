4befa1415e72e64c5dd7213c33268d05b5901aad
tommyrharper <thomasrobertharper@gmail.com>
2022-05-29 13:35:38 +0100

    feat: mutable variable

--- src/vars.rs
+++ src/vars.rs
@@ -4,6 +4,11 @@
 
 pub fn run() {
   let name = "Tom";
+  let mut age = 25;
 
-  println!("My name is {}", name);
+  println!("My name is {} and I am {}", name, age);
+
+  age = 26;
+
+  println!("My name is {} and I am {}", name, age);
 }
