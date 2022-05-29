c27906a3807efa3acbde24a94584a55b51d7ba9d
tommyrharper <thomasrobertharper@gmail.com>
2022-05-29 19:51:07 +0100

    feat: get boolean from expression

--- src/types.rs
+++ src/types.rs
@@ -23,4 +23,12 @@ pub fn run() {
   // Find max size
   println!("Max i32: {}", std::i32::MAX);
   println!("Max i64: {}", std::i64::MAX);
+
+  // Boolean
+  let is_active: bool = true;
+
+  // Get boolean from expression
+  let is_greater = 10 > 5;
+
+  println!("{:?}", (x, y, z, is_active, is_greater));
 }
