2e95a9300374619583fedc739084f11bcc4e1a1e
tommyrharper <thomasrobertharper@gmail.com>
2022-05-29 13:16:22 +0100

    feat: add string interpolation

--- src/print.rs
+++ src/print.rs
@@ -1,3 +1,5 @@
 pub fn run() {
-  println!("Hello from the print.rs file!")
+  println!("Hello from the print.rs file! {} {}", 1, "interpolated");
+
+  println!("{0} is from {1} and {0} likes to {2}", "Tom", "London", "code")
 }
