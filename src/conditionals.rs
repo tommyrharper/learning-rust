dec0149bd965b185646a26133f44b714526e893b
tommyrharper <thomasrobertharper@gmail.com>
2022-06-01 20:41:20 +0100

    feat: conditionals.rs

---
+++ src/conditionals.rs
@@ -0,0 +1,12 @@
+//  Conditionals
+
+pub fn run() {
+  let age = 18;
+
+  // If/Else
+  if age >= 21 {
+    println!("Bartender: What would you like to drink?");
+  } else {
+    println!("Bartender: Sorry, you have to leave");
+  }
+}
