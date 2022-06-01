ac8186410cadf3cea5bd3127785c49ee7fb33760
tommyrharper <thomasrobertharper@gmail.com>
2022-06-01 22:40:24 +0100

    feat: add functions.rs

---
+++ src/functions.rs
@@ -0,0 +1,13 @@
+// Functions
+
+pub fn run() {
+  greeting("Hello", "Jane");
+}
+
+fn greeting(greet: &str, name: &str) {
+  println!("{} {}, nice to meet you!", greet, name);
+}
+
+fn add(n1: &i32, n2: &i32) -> i32 {
+  n1 + n2 
+}
