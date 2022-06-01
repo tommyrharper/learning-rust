3a7646365199cfb2b6cc667bbdec9d2cae177771
tommyrharper <thomasrobertharper@gmail.com>
2022-06-01 20:52:49 +0100

    feat: add for in range loop

--- src/loops.rs
+++ src/loops.rs
@@ -3,29 +3,42 @@
 pub fn run() {
   let mut count = 0;
 
-  // Infinite loop
-  loop {
-    count += 1;
-    println!("Number: {}", count);
+  // // Infinite loop
+  // loop {
+  //   count += 1;
+  //   println!("Number: {}", count);
 
-    if count == 20 {
-      break;
-    }
-  }
+  //   if count == 20 {
+  //     break;
+  //   }
+  // }
 
-  count = 0;
+  // count = 0;
 
-  // While loop (FizzBuzz)
-  while count <= 100 {
-    if count % 15 == 0 {
+  // // While loop (FizzBuzz)
+  // while count <= 100 {
+  //   if count % 15 == 0 {
+  //     println!("fizzbuzz");
+  //   } else if count % 5 == 0 {
+  //     println!("buzz");
+  //   } else if count % 3 == 0 {
+  //     println!("fizz");
+  //   }  else {
+  //     println!("{}", count);
+  //   }
+  //   count += 1
+  // }
+
+  // For range loop
+  for x in 0..100 {
+    if x % 15 == 0 {
       println!("fizzbuzz");
-    } else if count % 5 == 0 {
+    } else if x % 5 == 0 {
       println!("buzz");
-    } else if count % 3 == 0 {
+    } else if x % 3 == 0 {
       println!("fizz");
     }  else {
-      println!("{}", count);
+      println!("{}", x);
     }
-    count += 1
   }
 }
