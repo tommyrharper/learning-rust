e2bef9c27bc1ae681b35c0a14ed02955e511bc11
tommyrharper <thomasrobertharper@gmail.com>
2022-06-01 20:45:14 +0100

    feat: add or operator in conditionals

--- src/conditionals.rs
+++ src/conditionals.rs
@@ -1,12 +1,20 @@
 //  Conditionals
 
 pub fn run() {
-  let age = 18;
+  let age: u8 = 30;
+  let check_id: bool = false;
+  let knows_person_of_age = true;
 
   // If/Else
-  if age >= 21 {
+  if age >= 21 && check_id || knows_person_of_age {
     println!("Bartender: What would you like to drink?");
-  } else {
+  } else if age < 21 && check_id {
     println!("Bartender: Sorry, you have to leave");
+  } else {
+    println!("Bartender: I'll need to see your ID");
   }
+
+  // Shorthand If
+  let is_of_age = if age >= 21 { true } else { false };
+  println!("Is of age: {}", is_of_age);
 }
