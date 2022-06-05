628d2296d5aa06ae45f34d512f57d069df2bd3f8
tommyrharper <thomasrobertharper@gmail.com>
2022-06-05 15:45:58 +0100

    feat: closure function

--- src/functions.rs
+++ src/functions.rs
@@ -6,6 +6,11 @@ pub fn run() {
   // Bind function values to variables
   let get_sum = add(5, 5);
   println!("Sum: {}", get_sum);
+
+  // Closure
+  let n3: i32 = 10;
+  let add_nums = |n1: i32, n2: i32| n1 + n2 + n3;
+  println!("C Sum: {}", add_nums(3, 3));
 }
 
 fn greeting(greet: &str, name: &str) {
